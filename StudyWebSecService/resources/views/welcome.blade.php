e216f0fce93939ea25ee517d28720b515cbf9297
Omar Abduh <89998959+Omar-Abduh@users.noreply.github.com>
2025-02-18 09:59:57 +0200

    edit in welcome

--- StudyWebSecService/resources/views/welcome.blade.php
+++ StudyWebSecService/resources/views/welcome.blade.php
@@ -18,157 +18,23 @@
         <div class="card-header">Multiplication Table</div>
         <div class="card-body">
             <div class="row">
-                <div class="card m-4 col-sm-2">
-                    @php($j = 1)
-                    <div class="card-header">{{ $j }} Multiplication Table</div>
-                    <div class="card-body">
-                        <table>
-                            @foreach (range(1, 10) as $i)
-                                <tr>
-                                    <td>{{ $i }} * {{ $j }}</td>
-                                    <td> = {{ $i * $j }}</td>
-                                    </li>
-                            @endforeach
-                        </table>
+                @foreach (range(1, 12) as $j)
+                    <div class="col-sm-2">
+                        <div class="card m-2">
+                            <div class="card-header">{{ $j }} Multiplication Table</div>
+                            <div class="card-body">
+                                <table>
+                                    @foreach (range(1, 10) as $i)
+                                        <tr>
+                                            <td>{{ $i }} * {{ $j }}</td>
+                                            <td>= {{ $i * $j }}</td>
+                                        </tr>
+                                    @endforeach
+                                </table>
+                            </div>
+                        </div>
                     </div>
-                </div>
-
-                <div class="card m-4 col-sm-2">
-                    @php($j = 2)
-                    <div class="card-header">{{ $j }} Multiplication Table</div>
-                    <div class="card-body">
-                        <table>
-                            @foreach (range(1, 10) as $i)
-                                <tr>
-                                    <td>{{ $i }} * {{ $j }}</td>
-                                    <td> = {{ $i * $j }}</td>
-                                    </li>
-                            @endforeach
-                        </table>
-                    </div>
-                </div>
-                
-                <div class="card m-4 col-sm-2">
-                    @php($j = 3)
-                    <div class="card-header">{{ $j }} Multiplication Table</div>
-                    <div class="card-body">
-                        <table>
-                            @foreach (range(1, 10) as $i)
-                                <tr>
-                                    <td>{{ $i }} * {{ $j }}</td>
-                                    <td> = {{ $i * $j }}</td>
-                                    </li>
-                            @endforeach
-                        </table>
-                    </div>
-                </div>
-
-                <div class="card m-4 col-sm-2">
-                    @php($j = 4)
-                    <div class="card-header">{{ $j }} Multiplication Table</div>
-                    <div class="card-body">
-                        <table>
-                            @foreach (range(1, 10) as $i)
-                                <tr>
-                                    <td>{{ $i }} * {{ $j }}</td>
-                                    <td> = {{ $i * $j }}</td>
-                                    </li>
-                            @endforeach
-                        </table>
-                    </div>
-                </div>
-
-                <div class="card m-4 col-sm-2">
-                    @php($j = 5)
-                    <div class="card-header">{{ $j }} Multiplication Table</div>
-                    <div class="card-body">
-                        <table>
-                            @foreach (range(1, 10) as $i)
-                                <tr>
-                                    <td>{{ $i }} * {{ $j }}</td>
-                                    <td> = {{ $i * $j }}</td>
-                                    </li>
-                            @endforeach
-                        </table>
-                    </div>
-                </div>
-
-                <div class="card m-4 col-sm-2">
-                    @php($j = 6)
-                    <div class="card-header">{{ $j }} Multiplication Table</div>
-                    <div class="card-body">
-                        <table>
-                            @foreach (range(1, 10) as $i)
-                                <tr>
-                                    <td>{{ $i }} * {{ $j }}</td>
-                                    <td> = {{ $i * $j }}</td>
-                                    </li>
-                            @endforeach
-                        </table>
-                    </div>
-                </div>
-                
-                <div class="card m-4 col-sm-2">
-                    @php($j = 7)
-                    <div class="card-header">{{ $j }} Multiplication Table</div>
-                    <div class="card-body">
-                        <table>
-                            @foreach (range(1, 10) as $i)
-                                <tr>
-                                    <td>{{ $i }} * {{ $j }}</td>
-                                    <td> = {{ $i * $j }}</td>
-                                    </li>
-                            @endforeach
-                        </table>
-                    </div>
-                </div>
-                
-                <div class="card m-4 col-sm-2">
-                    @php($j = 8)
-                    <div class="card-header">{{ $j }} Multiplication Table</div>
-                    <div class="card-body">
-                        <table>
-                            @foreach (range(1, 10) as $i)
-                                <tr>
-                                    <td>{{ $i }} * {{ $j }}</td>
-                                    <td> = {{ $i * $j }}</td>
-                                    </li>
-                            @endforeach
-                        </table>
-                    </div>
-                </div>
-                
-                <div class="card m-4 col-sm-2">
-                    @php($j = 9)
-                    <div class="card-header">{{ $j }} Multiplication Table</div>
-                    <div class="card-body">
-                        <table>
-                            @foreach (range(1, 10) as $i)
-                                <tr>
-                                    <td>{{ $i }} * {{ $j }}</td>
-                                    <td> = {{ $i * $j }}</td>
-                                    </li>
-                            @endforeach
-                        </table>
-                    </div>
-                </div>
-                
-                <div class="card m-4 col-sm-2">
-                    @php($j = 10)
-                    <div class="card-header">{{ $j }} Multiplication Table</div>
-                    <div class="card-body">
-                        <table>
-                            @foreach (range(1, 10) as $i)
-                                <tr>
-                                    <td>{{ $i }} * {{ $j }}</td>
-                                    <td> = {{ $i * $j }}</td>
-                                    </li>
-                            @endforeach
-                        </table>
-                    </div>
-                </div>
-                
-
+                @endforeach
             </div>
         </div>
     </div>
