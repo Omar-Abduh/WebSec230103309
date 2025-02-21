347b7feb3039b3508632967880360ebc126bbcd2
Omar Abduh <89998959+Omar-Abduh@users.noreply.github.com>
2025-02-21 22:23:39 +0200

    Update even number

--- WebSecService/resources/views/even_number.blade.php
+++ WebSecService/resources/views/even_number.blade.php
@@ -1,17 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.master')
+@section('title', 'Even Numbers')
+@section('content')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="public/css/bootstrap.min.css">
-    <script src="public/js/bootstrap.min.js"></script>
-    <title>Event Number</title>
-</head>
-
-<body>
-    <div class="card">
+    <div class="card m-4">
         <div class="card-header">Even Numbers</div>
         <div class="card-body">
             @foreach (range(1, 100) as $i)
@@ -23,6 +14,4 @@
             @endforeach
         </div>
     </div>
-</body>
-
-</html>
+@endsection
