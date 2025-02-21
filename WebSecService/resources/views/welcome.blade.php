4b77cdc26fe2ef7dfff262d1c3aa05eb3be23f1d
Omar Abduh <89998959+Omar-Abduh@users.noreply.github.com>
2025-02-21 22:27:17 +0200

    Update home page

--- WebSecService/resources/views/welcome.blade.php
+++ WebSecService/resources/views/welcome.blade.php
@@ -1,43 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Bootstrap Test</title>
-    <link rel="stylesheet" href="public/css/bootstrap.min.css">
-    <script src="public/js/bootstrap.min.js"></script>
-</head>
-
-<body>
-    <div class="" style="text-align: center; font-size: 50px; font-weight:bold">
-        Multiplication Table
-    </div>
-    <div class="card m-2">
-        <div class="card-header">Multiplication Table</div>
-        <div class="card-body">
-            <div class="row">
-                @foreach (range(1, 12) as $j)
-                    <div class="col-sm-2">
-                        <div class="card m-2">
-                            <div class="card-header">{{ $j }} Multiplication Table</div>
-                            <div class="card-body">
-                                <table>
-                                    @foreach (range(1, 10) as $i)
-                                        <tr>
-                                            <td>{{ $i }} * {{ $j }}</td>
-                                            <td>= {{ $i * $j }}</td>
-                                        </tr>
-                                    @endforeach
-                                </table>
-                            </div>
-                        </div>
-                    </div>
-                @endforeach
-            </div>
+@extends('layouts.master')
+@section('title', 'Home')
+@section('content')
+    <h1 style="text-align: center">Welcome to WebSec</h1>
+    <div class="card m-4">
+        <div class="card-body" style="text-align: center">
+            <button type="button" class="btn btn-primary" onclick="doSomething()">Press Me</button>
         </div>
-    </div>
-</body>
 
-</html>
+    </div>
+    <script>
+        function doSomething() {
+            alert('Hello From JavaScript');
+        }
+    </script>
+@endsection
