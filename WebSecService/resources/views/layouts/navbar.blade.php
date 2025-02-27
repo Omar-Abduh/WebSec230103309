15a73b89b383a375de518c8033d5c8463458f20f
Omar Abduh <89998959+Omar-Abduh@users.noreply.github.com>
2025-02-27 13:18:57 +0200

    Add products route to navbar and make list page

--- WebSecService/resources/views/layouts/navbar.blade.php
+++ WebSecService/resources/views/layouts/navbar.blade.php
@@ -19,6 +19,9 @@
             <li class="nav-item">
                 <a class="nav-link" href="{{ route('gpa') }}">GPA Task</a>
             </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('products.index') }}">Products</a>
+            </li>
         </ul>
     </div>
 </nav>
