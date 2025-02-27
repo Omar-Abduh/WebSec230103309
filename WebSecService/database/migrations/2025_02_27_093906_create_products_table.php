1703e844bd66f223d3f66caac689ffd5fe86e2cf
Omar Abduh <89998959+Omar-Abduh@users.noreply.github.com>
2025-02-27 15:39:34 +0200

    create the tables and rows

--- WebSecService/database/migrations/2025_02_27_093906_create_products_table.php
+++ WebSecService/database/migrations/2025_02_27_093906_create_products_table.php
@@ -13,7 +13,14 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('code', 64)->unique(); // 'code' column with a maximum length of 64 characters
+            $table->string('name', 256); // 'name' column with a maximum length of 256 characters
+            $table->unsignedBigInteger('price'); // 'price' column as an unsigned big integer
+            $table->string('model', 128); // 'model' column with a maximum length of 128 characters
+            $table->text('description')->nullable(); // 'description' column that can store longer text and can be null
+            $table->string('photo', 128)->nullable(); // 'photo' column with a maximum length of 128 characters and can be null
             $table->timestamps();
+            $table->softDeletes(); // This will create a 'deleted_at' column for soft deletes
         });
     }
 
