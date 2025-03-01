0a1e0a30e34a00a10a6dc5f2c4c946e4644b8f15
Omar Abduh <89998959+Omar-Abduh@users.noreply.github.com>
2025-03-01 22:15:24 +0200

    update the navbar

--- WebSecService/resources/views/layouts/navbar.blade.php
+++ WebSecService/resources/views/layouts/navbar.blade.php
@@ -4,24 +4,38 @@
             <li class="nav-item">
                 <a class="nav-link" href="{{ route('home') }}">Home</a>
             </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('even') }}">Even Numbers</a>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" id="lecture1Dropdown" role="button">
+                    Lecture 1
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="lecture1Dropdown">
+                    <li><a class="dropdown-item" href="{{ route('even') }}">Even Numbers</a></li>
+                    <li><a class="dropdown-item" href="{{ route('prime') }}">Prime Numbers</a></li>
+                    <li><a class="dropdown-item" href="{{ route('multable') }}">Multiplication Table</a></li>
+                    <li><a class="dropdown-item" href="{{ route('mini-test') }}">Bill Information</a></li>
+                    <li><a class="dropdown-item" href="{{ route('gpa') }}">GPA Task</a></li>
+                </ul>
             </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('prime') }}">Prime Numbers</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('multable') }}">Multiplication Table</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('mini-test') }}">Bill Information</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('gpa') }}">GPA Task</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('products.index') }}">Products</a>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" id="lecture2Dropdown" role="button">
+                    Lecture 2
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="lecture2Dropdown">
+                    <li><a class="dropdown-item" href="{{ route('products.index') }}">Products</a></li>
+                </ul>
             </li>
         </ul>
     </div>
 </nav>
+
+<style>
+    /* Show dropdown on hover */
+    .nav-item.dropdown:hover .dropdown-menu {
+        display: block;
+    }
+
+    /* Ensure the dropdown stays visible */
+    .nav-item .dropdown-toggle::after {
+        display: none;
+    }
+</style>
