47acda0dd3b56fdb3383a3c625f8fffc975cbbcf
Omar Abduh <89998959+Omar-Abduh@users.noreply.github.com>
2025-03-01 21:46:42 +0200

    Update the desgin and edit it titles in edit.page

--- WebSecService/resources/views/lec2/products/edit.blade.php
+++ WebSecService/resources/views/lec2/products/edit.blade.php
@@ -1,45 +1,51 @@
 @extends('layouts.master')
-@section('title', 'Edit Product')
+@section('title', ($products->id ? 'Edit' : 'Add') . ' Product')
 @section('content')
-    <form action="{{ route('products.save', $products->id ?? '') }}" method="post">
-        {{ csrf_field() }}
-        <div class="row mb-2">
-            <div class="col-6">
-                <label for="code" class="form-label">Code:</label>
-                <input type="text" class="form-control" placeholder="Code" name="code" required
-                    value="{{ $products->code }}">
-            </div>
-            <div class="col-6">
-                <label for="model" class="form-label">Model:</label>
-                <input type="text" class="form-control" placeholder="Model" name="model" required
-                    value="{{ $products->model }}">
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">
+                        {{ $products->id ? 'Edit' : 'Add' }} Product
+                    </div>
+
+                    <div class="card-body">
+                        <form action="{{ route('products.save', $products->id ?? '') }}" method="post">
+                            {{ csrf_field() }}
+                            <div class="mb-3">
+                                <label for="code" class="form-label">Code:</label>
+                                <input type="text" class="form-control" placeholder="Code" name="code" required
+                                    value="{{ $products->code }}">
+                            </div>
+                            <div class="mb-3">
+                                <label for="model" class="form-label">Model:</label>
+                                <input type="text" class="form-control" placeholder="Model" name="model" required
+                                    value="{{ $products->model }}">
+                            </div>
+                            <div class="mb-3">
+                                <label for="name" class="form-label">Name:</label>
+                                <input type="text" class="form-control" placeholder="Name" name="name" required
+                                    value="{{ $products->name }}">
+                            </div>
+                            <div class="mb-3">
+                                <label for="price" class="form-label">Price:</label>
+                                <input type="number" class="form-control" placeholder="Price" name="price" required
+                                    value="{{ $products->price }}">
+                            </div>
+                            <div class="mb-3">
+                                <label for="photo" class="form-label">Photo:</label>
+                                <input type="text" class="form-control" placeholder="Photo" name="photo" required
+                                    value="{{ $products->photo }}">
+                            </div>
+                            <div class="mb-3">
+                                <label for="description" class="form-label">Description:</label>
+                                <textarea class="form-control" placeholder="Description" name="description" required>{{ $products->description }}</textarea>
+                            </div>
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </form>
+                    </div>
+                </div>
             </div>
         </div>
-        <div class="row mb-2">
-            <div class="col">
-                <label for="name" class="form-label">Name:</label>
-                <input type="text" class="form-control" placeholder="Name" name="name" required
-                    value="{{ $products->name }}">
-            </div>
-        </div>
-        <div class="row mb-2">
-            <div class="col-6">
-                <label for="model" class="form-label">Price:</label>
-                <input type="number" class="form-control" placeholder="Price" name="price" required
-                    value="{{ $products->price }}">
-            </div>
-            <div class="col-6">
-                <label for="model" class="form-label">Photo:</label>
-                <input type="text" class="form-control" placeholder="Photo" name="photo" required
-                    value="{{ $products->photo }}">
-            </div>
-        </div>
-        <div class="row mb-2">
-            <div class="col">
-                <label for="name" class="form-label">Description:</label>
-                <textarea class="form-control" placeholder="Description" name="description" required>{{ $products->description }}</textarea>
-            </div>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+    </div>
 @endsection
