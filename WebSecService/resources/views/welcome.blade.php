d0b3d59218b57ffebb0197406fe0fa06f5fb27fe
Omar Abduh <89998959+Omar-Abduh@users.noreply.github.com>
2025-03-01 22:16:59 +0200

    update the home page

--- WebSecService/resources/views/welcome.blade.php
+++ WebSecService/resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.master')
 @section('title', 'Home')
 @section('content')
-    <h1 style="text-align: center">Welcome to WebSec</h1>
+    <h1 class="mt-3" style="text-align: center">Welcome to WebSec</h1>
     <div class="contiainer m-3">
         <div class="card-body" style="text-align: center">
             <button type="button" class="btn btn-primary" onclick="doSomething()">Press Me</button>
