8ed1f6236546d4860c182a696e24f528b195f5a3
Omar Abduh <omarmohamedabdou@outlook.com>
2025-03-23 10:24:52 +0200

    edit in users view (admin)

--- WebSecService/resources/views/users/index.blade.php
+++ WebSecService/resources/views/users/index.blade.php
@@ -19,30 +19,53 @@
                     <table class="table table-striped table-hover">
                         <thead class="table-light">
                             <tr>
-                                <th>#</th>
-                                <th>Name</th>
-                                <th>Email</th>
-                                <th>Created At</th>
-                                <th>Updated At</th>
-                                <th>Show Profile</th>
-                                <th>Actions</th>
+                                <th scope="col">#</th>
+                                <th scope="col">Name</th>
+                                <th scope="col">Email</th>
+                                <th scope="col">Roles</th>
+                                <th scope="col">Created At</th>
+                                <th scope="col">Updated At</th>
+                                <th scope="col">Show Profile</th>
+                                <th scope="col">Actions</th>
                             </tr>
                         </thead>
                         <tbody>
-                            @foreach($users as $user)
+                            @foreach ($users as $user)
                                 <tr>
-                                    <td>{{ $user->id }}</td>
-                                    <td>{{ $user->name }}</td>
-                                    <td>{{ $user->email }}</td>
-                                    <td>{{ $user->created_at }}</td>
-                                    <td>{{ $user->updated_at }}</td>
-                                    <td>
+                                    <td scope="col">{{ $user->id }}</td>
+                                    <td scope="col">{{ $user->name }}</td>
+                                    <td scope="col">{{ $user->email }}</td>
+                                    <td scope="col">
+                                        @if ($user->getRoleNames()->isEmpty())
+                                            <a href="">
+                                                <span class="badge bg-secondary">No Role</span>
+                                            </a>
+                                        @else
+                                            @foreach ($user->getRoleNames() as $roleName)
+                                                @if ($roleName == 'Admin')
+                                                    <a href="{{ route('roles.index') }}">
+                                                        <span class="badge bg-danger">{{ $roleName }}</span>
+                                                    </a>
+                                                @else
+                                                    <a href="{{ route('roles.index') }}">
+                                                        <span class="badge bg-primary">{{ $roleName }}</span>
+                                                    </a>
+                                                @endif
+                                            @endforeach
+                                        @endif
+                                    </td>
+                                    <td scope="col">{{ $user->created_at->format('d M Y') }}</td>
+                                    <td scope="col">{{ $user->updated_at->format('d M Y H:i:s') }}</td>
+                                    <td scope="col">
                                         <a href="{{ route('users.profile', $user->id) }}">Show {{ $user->name }}</a>
                                     </td>
-                                    <td>
-                                        <a href="{{ route('users.edit', $user->id) }}" class="btn btn-warning btn-sm" >Edit</a>
-                                        <a href="{{ route('users.change_pass', $user->id) }}" class="btn btn-warning btn-sm" >Change Password</a>
-                                        <a href="{{ route('users.destroy', $user->id) }}" class="btn btn-danger btn-sm" >Delete</a>
+                                    <td scope="col">
+                                        <a href="{{ route('users.edit', $user->id) }}"
+                                            class="btn btn-warning btn-sm">Edit</a>
+                                        <a href="{{ route('users.change_pass', $user->id) }}"
+                                            class="btn btn-warning btn-sm">Change Password</a>
+                                        <a href="{{ route('users.destroy', $user->id) }}"
+                                            class="btn btn-danger btn-sm">Delete</a>
                                     </td>
                                 </tr>
                             @endforeach
