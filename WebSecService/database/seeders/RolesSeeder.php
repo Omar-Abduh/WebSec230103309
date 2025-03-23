020876357bebd992d99af4e6858565b67adcb769
Omar Abduh <omarmohamedabdou@outlook.com>
2025-03-23 14:09:42 +0200

    Update permissions and add Roles, Permissions

--- WebSecService/database/seeders/RolesSeeder.php
+++ WebSecService/database/seeders/RolesSeeder.php
@@ -17,19 +17,30 @@ class RolesSeeder extends Seeder
     {
         $role_admin = Role::create(['name' => 'Admin']);
         $role_employee = Role::create(['name' => 'Employee']);
-        $permission_admin = Permission::create(['name' => 'add_products', 'display_name' => 'Add Products']);
-        $permission_admin = Permission::create(['name' => 'edit_products', 'display_name' => 'Edit Products']);
-        $permission_admin = Permission::create(['name' => 'delete_products', 'display_name' => 'Delete Products']);
-        $permission_admin = Permission::create(['name' => 'show_users', 'display_name' => 'Show Users']);
-        $permission_admin = Permission::create(['name' => 'edit_users', 'display_name' => 'Edit Users']);
-        $permission_admin = Permission::create(['name' => 'delete_users', 'display_name' => 'Delete Users']);
-        $permission_admin = Permission::create(['name' => 'admin_users', 'display_name' => 'Admin Users']);
-        $permission_admin = Permission::create(['name' => 'add_users', 'display_name' => 'Add Users']);
+        Permission::create(['name' => 'add_products', 'display_name' => 'Add Products']);
+        Permission::create(['name' => 'edit_products', 'display_name' => 'Edit Products']);
+        Permission::create(['name' => 'delete_products', 'display_name' => 'Delete Products']);
+        Permission::create(['name' => 'show_users', 'display_name' => 'Show Users']);
+        Permission::create(['name' => 'edit_users', 'display_name' => 'Edit Users']);
+        Permission::create(['name' => 'delete_users', 'display_name' => 'Delete Users']);
+        Permission::create(['name' => 'admin_users', 'display_name' => 'Admin Users']);
+        Permission::create(['name' => 'add_users', 'display_name' => 'Add Users']);
+        Permission::create(['name' => 'add_roles', 'display_name' => 'Add Roles']);
+        Permission::create(['name' => 'add_permissions', 'display_name' => 'Add Permissions']);
+        Permission::create(['name' => 'edit_roles', 'display_name' => 'Edit Roles']);
+        Permission::create(['name' => 'edit_permissions', 'display_name' => 'Edit Permissions']);
+        Permission::create(['name' => 'delete_roles', 'display_name' => 'Delete Roles']);
+        Permission::create(['name' => 'delete_permissions', 'display_name' => 'Delete Permissions']);
+        Permission::create(['name' => 'show_roles', 'display_name' => 'Show Roles']);
+        Permission::create(['name' => 'show_permissions', 'display_name' => 'Show Permissions']);
         $role_admin->givePermissionTo(['add_products', 'edit_products', 'delete_products', 'show_users', 'edit_users', 'delete_users', 'admin_users', 'add_users']);
 
         $admin_user = User::find(1);
         $test_user = User::find(2);
         $admin_user->assignRole('Admin');
         $test_user->assignRole('Employee');
+
+        // Direct Permissions
+        $admin_user->givePermissionTo(['show_roles', 'show_permissions', 'add_roles', 'add_permissions', 'edit_roles', 'edit_permissions', 'delete_roles', 'delete_permissions']);
     }
 }
