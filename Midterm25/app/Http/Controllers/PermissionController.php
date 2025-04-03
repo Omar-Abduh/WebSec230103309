4a07b5656987df531b2b2ccfab48419d327c3887
Omar-Abduh <omarmohamedabdou@outlook.com>
2025-04-03 11:25:55 +0200

    do the permissions CRUD

---
+++ Midterm25/app/Http/Controllers/PermissionController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Spatie\Permission\Models\Permission;
+
+class PermissionController extends Controller
+{
+    public function index()
+    {
+        $permissions = Permission::paginate(8);
+        $permissions_total = Permission::all();
+
+        return view('admin.permissions.index', compact('permissions', 'permissions_total'));
+    }
+    
+    public function show()
+    {
+        $permissions = Permission::all();
+
+        return view('admin.permissions.show', compact('permissions'));
+    }
+
+    // Create permission
+    public function create()
+    {
+        return view('admin.permissions.create');
+    }
+
+    // Store permission
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|unique:permissions,name',
+        ]);
+
+        permission::create(['name' => $request->name]);
+
+        return redirect()->route('permission.index')->with([
+            'success' => 'permission created successfully.',
+            'success_description' => 'The permission has been added to the system.'
+        ]);
+    }
+
+    // Edit permission
+    public function edit(Permission $permission)
+    {
+        return view('admin.permissions.edit', compact('permission'));
+    }
+
+    // Update permission
+    public function update(Request $request, Permission $permission)
+    {
+        $request->validate([
+            'name' => 'required|unique:permissions,name,' . $permission->id,
+        ]);
+
+        $permission->update(['name' => $request->name]);
+
+        return redirect()->route('permission.index')->with([
+            'success' => 'permission updated successfully.',
+            'success_description' => 'The permission details have been updated.'
+        ]);
+    }
+
+    // Delete permission
+    public function destroy(Permission $permission)
+    {
+        $permission->delete();
+        return redirect()->back()->with([
+            'success' => 'permission deleted successfully.',
+            'success_description' => 'The permission has been removed from the system.'
+        ]);
+    }
+}
--- Midterm25/routes/web.php
+++ Midterm25/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PermissionController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\RoleController;
 use Illuminate\Support\Facades\Route;
@@ -29,11 +30,20 @@ Route::middleware('auth')->group(function () {
 });
 
 Route::middleware(['auth', 'admin'])->group(function () {
+    // Roles
     Route::get('/roles', [RoleController::class, 'index'])->name('role.index');
     Route::get('/role/create', [RoleController::class, 'create'])->name('role.create');
     Route::post('/role/create', [RoleController::class, 'store'])->name('role.store');
     Route::get('/role/edit/{role}', [RoleController::class, 'edit'])->name('role.edit');
     Route::patch('/role/edit/{role}', [RoleController::class, 'update'])->name('role.update');
     Route::delete('/roles/{role}', [RoleController::class, 'destroy'])->name('role.delete');
+    // Permissions
+    Route::get('/permissions', [PermissionController::class, 'index'])->name('permission.index');
+    Route::get('/permissions/show', [PermissionController::class, 'show'])->name('permission.show');
+    Route::get('/permission/create', [PermissionController::class, 'create'])->name('permission.create');
+    Route::post('/permission/create', [PermissionController::class, 'store'])->name('permission.store');
+    Route::get('/permission/edit/{permission}', [PermissionController::class, 'edit'])->name('permission.edit');
+    Route::patch('/permission/edit/{permission}', [PermissionController::class, 'update'])->name('permission.update');
+    Route::delete('/permissions/{permission}', [PermissionController::class, 'destroy'])->name('permission.delete');
 });
 require __DIR__ . '/auth.php';
