5e414f4582a11a48f5d14d177da9a4cfed926f08
Omar-Abduh <omarmohamedabdou@outlook.com>
2025-04-05 01:53:11 +0200

    remove edit and delete icons from assign permissions to role view

--- Midterm25/resources/views/admin/role/permissions.blade.php
+++ Midterm25/resources/views/admin/role/permissions.blade.php
@@ -74,19 +74,6 @@
 
                                         </button>
                                     </form>
-                                    <a href="{{ route('permission.edit', $permission->id) }}">
-                                        <button
-                                            class="p-2 bg-gray-200 dark:bg-gray-700 rounded-lg hover:bg-gray-300 dark:hover:bg-gray-600">
-                                            <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4" viewBox="0 0 24 24"
-                                                fill="currentColor">
-                                                <path
-                                                    d="M21.731 2.269a2.625 2.625 0 00-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 000-3.712zM19.513 8.199l-3.712-3.712-12.15 12.15a5.25 5.25 0 00-1.32 2.214l-.8 2.685a.75.75 0 00.933.933l2.685-.800a5.25 5.25 0 002.214-1.32L19.513 8.2z">
-                                                </path>
-                                            </svg>
-                                        </button>
-                                    </a>
-                                    <x-delete-modal item="Permission: {{ $permission->name }}"
-                                        action="{{ route('permission.delete', $permission->id) }}" />
                                 </td>
                             </tr>
                         @endforeach
