e3a1789376ca73df287c02ef6a41a03c8f31d973
Omar-Abduh <omarmohamedabdou@outlook.com>
2025-04-04 02:54:24 +0200

    add Access Control Panel btn in navbar

--- Midterm25/resources/views/layouts/navigation.blade.php
+++ Midterm25/resources/views/layouts/navigation.blade.php
@@ -16,6 +16,11 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('access-control-panel')" :active="request()->routeIs('access-control-panel')">
+                        {{ __('Access Control Panel') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
