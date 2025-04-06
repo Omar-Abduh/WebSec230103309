e3a6017fb267baeebca0e0a744fd7e6e1967749d
Omar-Abduh <omarmohamedabdou@outlook.com>
2025-04-06 17:30:06 +0200

    Do View validation with permissions

--- Midterm25/resources/views/access-control-panel.blade.php
+++ Midterm25/resources/views/access-control-panel.blade.php
@@ -21,7 +21,8 @@
             <div class="flex justify-center items-center mb-4">
                 <div>
                     <h3 class="text-lg font-semibold dark:text-gray-200">Access Control Panel Dashboard</h3>
-                    <p class="text-gray-500 dark:text-gray-400">Welcome to Access Control Panel {{ Auth::user()->name }} 👋
+                    <p class="text-gray-500 dark:text-gray-400">Welcome to Access Control Panel {{ Auth::user()->name }}
+                        👋
                     </p>
                 </div>
             </div>
@@ -35,50 +36,62 @@
                         </tr>
                     </thead>
                     <tbody class="text-white">
-                        <tr class="border-t dark:border-gray-600 hover:bg-gray-100 dark:hover:bg-gray-700">
-    
-                            <td class="p-4">User Access Control</td>
-                            <td class="p-4">{{ $users }}</td>
-                            <td class="p-4">
-                                <a href="{{ route('user.index') }}">
-                                    <button
-                                        class="p-2 bg-gray-200 dark:bg-gray-700 rounded-lg hover:bg-gray-300 dark:hover:bg-gray-600">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
-                                            <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
-                                        </svg>
-                                    </button>
-                                </a>
-                            </td>
-                        </tr>
-                        <tr class="border-t dark:border-gray-600 hover:bg-gray-100 dark:hover:bg-gray-700">
+                        @haspermission('Show Users')
+                            <tr class="border-t dark:border-gray-600 hover:bg-gray-100 dark:hover:bg-gray-700">
 
-                            <td class="p-4">Roles Management</td>
-                            <td class="p-4">{{ $roles }}</td>
-                            <td class="p-4">
-                                <a href="{{ route('role.index') }}">
-                                    <button
-                                        class="p-2 bg-gray-200 dark:bg-gray-700 rounded-lg hover:bg-gray-300 dark:hover:bg-gray-600">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
-                                            <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
-                                        </svg>
-                                    </button>
-                                </a>
-                            </td>
-                        </tr>
-                        <tr class="border-t dark:border-gray-600 hover:bg-gray-100 dark:hover:bg-gray-700">
+                                <td class="p-4">User Access Control</td>
+                                <td class="p-4">{{ $users }}</td>
+                                <td class="p-4">
+                                    <a href="{{ route('user.index') }}">
+                                        <button
+                                            class="p-2 bg-gray-200 dark:bg-gray-700 rounded-lg hover:bg-gray-300 dark:hover:bg-gray-600">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                                stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                                                <path stroke-linecap="round" stroke-linejoin="round"
+                                                    d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
+                                            </svg>
+                                        </button>
+                                    </a>
+                                </td>
+                            </tr>
+                        @endhaspermission
+                        @haspermission('Show Roles')
+                            <tr class="border-t dark:border-gray-600 hover:bg-gray-100 dark:hover:bg-gray-700">
 
-                            <td class="p-4">Permissions Management</td>
-                            <td class="p-4">{{ $permissions }}</td>
-                            <td class="p-4">
-                                <a href="{{ route('permission.index') }}">
-                                    <button
-                                        class="p-2 bg-gray-200 dark:bg-gray-700 rounded-lg hover:bg-gray-300 dark:hover:bg-gray-600">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
-                                            <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
-                                        </svg>
-                                    </button>
-                                </a>
-                            </td>
+                                <td class="p-4">Roles Management</td>
+                                <td class="p-4">{{ $roles }}</td>
+                                <td class="p-4">
+                                    <a href="{{ route('role.index') }}">
+                                        <button
+                                            class="p-2 bg-gray-200 dark:bg-gray-700 rounded-lg hover:bg-gray-300 dark:hover:bg-gray-600">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                                stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                                                <path stroke-linecap="round" stroke-linejoin="round"
+                                                    d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
+                                            </svg>
+                                        </button>
+                                    </a>
+                                </td>
+                            </tr>
+                        @endhaspermission
+                        @haspermission('Show Permissions')
+                            <tr class="border-t dark:border-gray-600 hover:bg-gray-100 dark:hover:bg-gray-700">
+
+                                <td class="p-4">Permissions Management</td>
+                                <td class="p-4">{{ $permissions }}</td>
+                                <td class="p-4">
+                                    <a href="{{ route('permission.index') }}">
+                                        <button
+                                            class="p-2 bg-gray-200 dark:bg-gray-700 rounded-lg hover:bg-gray-300 dark:hover:bg-gray-600">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                                stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                                                <path stroke-linecap="round" stroke-linejoin="round"
+                                                    d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
+                                            </svg>
+                                        </button>
+                                    </a>
+                                </td>
+                            @endhaspermission
                     </tbody>
                 </table>
             </div>
