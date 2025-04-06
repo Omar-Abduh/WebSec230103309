a93f35a39f67a220eca4fc939eca27c28677f7cd
Omar-Abduh <omarmohamedabdou@outlook.com>
2025-04-07 01:27:47 +0200

    update the insufficient file

--- WebSecService/resources/views/products/insufficient_credit.blade.php
+++ WebSecService/resources/views/products/insufficient_credit.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="text-center">
         <img src="{{ asset('images/errors/error.jpg') }}" alt="Error" width="50%">
-        <p>Your credit amount is <strong>{{ auth()->user()->credit->credit_amount.'$' }}</strong> and it's <strong>not enough</strong>.</p>
+        <p>Your credit amount is <strong>{{ auth()->user()->credit->credit_amount.'$' }}</strong> and it's <strong>not enough</strong>.<br>The Product is Price <strong>{{ $product->price }}</strong>.</p>
         <p>Contact Your Administrator</p>
         <a href="javascript:history.back()" class="btn btn-primary">Go Back</a>
     </div>
