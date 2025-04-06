e772136ecf3bf9234795bed46c303ec05532a41a
Omar-Abduh <omarmohamedabdou@outlook.com>
2025-04-07 01:31:01 +0200

    do product buy route

--- WebSecService/routes/web.php
+++ WebSecService/routes/web.php
@@ -29,6 +29,7 @@ Route::get('products', [ProductsController::class, 'list'])->name('products_list
 Route::get('products/edit/{product?}', [ProductsController::class, 'edit'])->name('products_edit');
 Route::post('products/save/{product?}', [ProductsController::class, 'save'])->name('products_save');
 Route::get('products/delete/{product}', [ProductsController::class, 'delete'])->name('products_delete');
+Route::get('products/buy/{product}', [ProductsController::class, 'buy'])->name('products.buy');
 
 Route::get('/', function () {
     return view('welcome');
